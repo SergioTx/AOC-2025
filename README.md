1fa69bed342923fe2eaf157068fff837d98862f5
Sergio Tx <txitxo37@gmail.com>
2025-12-03 00:22:12 +0100

    chore: add project config

---
+++ README.md
@@ -0,0 +1,3 @@
+# AOC-2025
+
+[Advent of Code 2025](https://adventofcode.com/) solved using Deno and TS.
---
+++ main.ts
@@ -0,0 +1,27 @@
+// import './src/day1/1.ts';
+// import './src/day2/2.ts';
+// import './src/day3/3.ts';
+// import './src/day4/4.ts';
+// import './src/day5/5.ts';
+// import './src/day6/6.ts';
+// import './src/day7/7.ts';
+// import './src/day8/8.ts';
+// import './src/day9/9.ts';
+
+// import './src/day10/10.ts';
+// import './src/day11/11.ts';
+// import './src/day12/12.ts';
+// import './src/day13/13.ts';
+// import './src/day14/14.ts';
+// import './src/day15/15.ts';
+// import './src/day16/16.ts';
+// import './src/day17/17.ts';
+// import './src/day18/18.ts';
+// import './src/day19/19.ts';
+
+// import './src/day20/20.ts';
+// import './src/day21/21.ts';
+// import './src/day22/22.ts';
+// import './src/day23/23.ts';
+// import './src/day24/24.ts';
+// import './src/day25/25.ts';

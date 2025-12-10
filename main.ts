80e245e7a8a5d8f1baa9c1f032f3bc46978a3d19
Sergio Tx <txitxo37@gmail.com>
2025-12-10 10:11:22 +0100

    chore: automate imports

--- main.ts
+++ main.ts
@@ -1,30 +1,9 @@
-console.time('AOC 2025 Total Time');
+function importFn(day: `day${number}`) {
+  return import(`./src/${day}/index.ts`).catch(() => {
+    // do nothing
+  });
+}
 
-import './src/day1/index.ts';
-import './src/day2/index.ts';
-import './src/day3/index.ts';
-import './src/day4/index.ts';
-import './src/day5/index.ts';
-import './src/day6/index.ts';
-import './src/day7/index.ts';
-// import './src/day8/index.ts';
-// import './src/day9/index.ts';
-
-// import './src/day10/index.ts';
-// import './src/day11/index.ts';
-// import './src/day12/index.ts';
-// import './src/day13/index.ts';
-// import './src/day14/index.ts';
-// import './src/day15/index.ts';
-// import './src/day16/index.ts';
-// import './src/day17/index.ts';
-// import './src/day18/index.ts';
-// import './src/day19/index.ts';
-
-// import './src/day20/index.ts';
-// import './src/day21/index.ts';
-// import './src/day22/index.ts';
-// import './src/day23/index.ts';
-// import './src/day24/index.ts';
-// import './src/day25/index.ts';
-console.timeEnd('AOC 2025 Total Time');
+for (let i = 1; i <= 12; i++) {
+  importFn(`day${i}`);
+}

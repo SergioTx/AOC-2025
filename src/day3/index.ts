96ec4ee92b5ee349b6a41307b3f3a945b5c51589
Sergio Tx <txitxo37@gmail.com>
2025-12-19 11:39:34 +0100

    feat: change params to enable running a full day

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -6,10 +6,18 @@ const input = file.split('\n');
 const day = import.meta.dirname?.match(/day(\d+)$/)?.at(1) ?? '-';
 
 console.log(`--- Day ${day} ---`);
-if (Deno.args.length === 0 || Deno.args.some((arg) => arg.toUpperCase() === `${day}A`)) {
+if (
+  Deno.args.length === 0 ||
+  Deno.args.some((arg) => arg === day) ||
+  Deno.args.some((arg) => arg.toUpperCase() === `${day}A`)
+) {
   console.log(`${day}A: ${solveA(input)}`);
 }
 
-if (Deno.args.length === 0 || Deno.args.some((arg) => arg.toUpperCase() === `${day}B`)) {
+if (
+  Deno.args.length === 0 ||
+  Deno.args.some((arg) => arg === day) ||
+  Deno.args.some((arg) => arg.toUpperCase() === `${day}B`)
+) {
   console.log(`${day}B: ${solveB(input)}`);
 }
